d82813fd84de18571d2f30cc23790fce2c464e56
diegoronrod <69993079+diegoronrod@users.noreply.github.com>
2023-11-13 16:38:27 -0300

    atualoização

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "a5njh3",
   e2e: {
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
